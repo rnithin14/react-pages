f103d3205ed4fbf78ea279623deb6038269778e7
Nithin R <nithin>
2022-02-22 14:31:38 +0530

    4th changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <h1>hello</h1>
+           <h1>hi</h1>
          
         </p>
         <a
